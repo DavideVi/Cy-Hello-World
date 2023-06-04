280f8e270266993279ea469da4ded2089de94e8e
Davide Vitelaru <davide@vitelaru.com>
2023-06-04 14:01:08 +0100

    fix(deploy): increase limits so that requests don't get throttled

--- deploy/main.tf
+++ deploy/main.tf
@@ -93,6 +93,8 @@ resource "aws_apigatewayv2_stage" "hello_world_stage" {
   default_route_settings {
     detailed_metrics_enabled = true
     logging_level = "INFO"
+    throttling_burst_limit = 5000
+    throttling_rate_limit = 10000
   }
 }
 
