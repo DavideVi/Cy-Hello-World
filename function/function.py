08a72dca14f16a4dc381e457fd2aab9b67358572
Davide Vitelaru <davide@vitelaru.com>
2023-06-04 14:34:01 +0100

    feat(function): enforce content-type and method

--- function/function.py
+++ function/function.py
@@ -1,6 +1,18 @@
 
 def lambda_handler(event, context):
     
+    if event.httpMethod != 'GET':
+        return {
+            'statusCode': 405,
+            'body': 'Method not allowed'
+        }
+    
+    if event.headers['Content-Type'] != 'application/html':
+        return {
+            'statusCode': 406,
+            'body': 'Only HTML is supported'
+        }
+
     response = {
         'statusCode': 200,
         'body': 'Hello World'
